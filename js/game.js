e5c83dc0353001eb02b533b55a44d05a0cff22d5
Paweł Płodzień <pawel.plodzien@gmail.com>
2019-04-24 09:23:15 +0200

    unecessary files deleted

--- js/game.js
+++ js/game.js
@@ -1,74 +1,130 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var ballRadius = 10;
-var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
-var paddleHeight = 10;
-var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth)/2;
-var rightPressed = false;
-var leftPressed = false;
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+var pong = {};
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
+
+
+
+function Paddle(name) {
+    this.x = 0;
+    this.y = 0;
+    this.width = 5;
+    this.height = 30;
+    this.name = name;
+    this.offset = 3;
+    this.keyUp = false;
+    this.keyDown = false;
+    this.draw = function() {
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = "red";
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+
+function initGame() {
+    pong.players = [];
+    pong.players[0] = new Paddle('Player 1');
+    pong.players[1] = new Paddle('Player 2');
+    setPositions();
+}
+
+
+
+function setPositions() {
+    pong.players[0].x = 0;
+    pong.players[1].x = canvas.width - pong.players[1].width;
+    pong.players[0].y = (canvas.height - pong.players[0].height)/2;
+    pong.players[1].y = (canvas.height - pong.players[1].height)/2;
+}
+
+
 
 function keyDownHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = true;
+    if (e.key === 'a') {
+        pong.players[0].keyUp = true;
     }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = true;
+
+    else if (e.key === 'k') {
+        pong.players[1].keyUp = true;
+    }
+
+    else if (e.key === 'z') {
+        pong.players[0].keyDown = true;
+    }
+
+    else if (e.key == 'm') {
+        pong.players[1].keyDown = true;
     }
 }
 
 function keyUpHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = false;
+    if (e.key === 'a') {
+        pong.players[0].keyUp = false;
     }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = false;
+
+    else if (e.key === 'k') {
+        pong.players[1].keyUp = false;
+    }
+
+    else if (e.key === 'z') {
+        pong.players[0].keyDown = false;
+    }
+
+    else if (e.key == 'm') {
+        pong.players[1].keyDown = false;
     }
 }
 
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
-function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
+
+
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawPaddle();
-    window.requestAnimationFrame(draw);
-
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-        dx = -dx;
-    }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-        dy = -dy;
-    }
-
-    if(rightPressed && paddleX < canvas.width-paddleWidth) {
-        paddleX += 7;
-    }
-    else if(leftPressed && paddleX > 0) {
-        paddleX -= 7;
-    }
-
-    x += dx;
-    y += dy;
+    pong.players[0].draw();
+    pong.players[1].draw();
 }
 
-draw();
+function moveElements() {
+    for (i = 0; i < pong.players.length; i++) {
+        if (pong.players[i].keyUp && pong.players[i].y > 0) {
+            pong.players[i].y -= pong.players[i].offset;
+        }
+
+        if (pong.players[i].keyDown && pong.players[i].y + pong.players[i].height < canvas.height) {
+            pong.players[i].y += pong.players[i].offset;
+        }
+
+    }
+}
+
+
+function play() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    draw();
+    moveElements();
+    requestAnimationFrame(play);
+}
+
+
+
+
+
+initGame();
+play();
+
+
+
+
+
+
+
+
+
+
+
+
+
